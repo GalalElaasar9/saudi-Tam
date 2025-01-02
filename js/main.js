a3aa72e8e3e1c25f1f42b58b4e11a994cb628fde
GalalElaasar9 <138381852+GalalElaasar9@users.noreply.github.com>
2025-01-02 23:32:24 +0200

    ..

--- js/main.js
+++ js/main.js
@@ -13,41 +13,33 @@ const selects = [
   },
 ];
 
-// تغيير اللغة عند اختيار خيار
 document.querySelectorAll('.option').forEach(option => {
   option.addEventListener('click', () => {
     const lang = option.dataset.lang;
     const flagSrc = option.querySelector('img').src;
     const languageText = option.querySelector('span').textContent;
 
-    // تحديث القوائم جميعها
     selects.forEach(select => {
       select.flag.src = flagSrc;
       select.language.textContent = languageText;
     });
 
-    // تخزين اللغة المختارة
     localStorage.setItem('language', lang);
 
-    // تنفيذ تغيير اللغة
     setLanguage(lang);
   });
 });
 
-// تطبيق اللغة المخزنة
 function setLanguage(lang) {
   if (lang === 'ar') {
     document.documentElement.lang = 'ar';
     document.documentElement.dir = 'rtl';
-    // alert('تم اختيار اللغة العربية');
   } else {
     document.documentElement.lang = 'en';
     document.documentElement.dir = 'ltr';
-    // alert('English selected');
   }
 }
 
-// استعادة اللغة المخزنة عند التحميل
 const savedLanguage = localStorage.getItem('language') || 'ar';
 const initialOption = document.querySelector(`.option[data-lang="${savedLanguage}"]`);
 if (initialOption) {
@@ -62,27 +54,19 @@ if (initialOption) {
   setLanguage(savedLanguage);
 }
 
-// let links = document.querySelectorAll('.nav-link')
-// links.forEach((link)=>{
-//   if(window.location.href === link.href){
-//     link.classList.add('active');
-//   }
-// })
 
-  // لضمان إضافة الفئة collapsed عند النقر
-  document.querySelector('.navbar-toggler').addEventListener('click', function () {
-    this.classList.toggle('collapsed');
-  });
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+  this.classList.toggle('collapsed');
+});
 
 
-// let 
-// let navbar = document.querySelector('.navbar');
-// window.addEventListener("scroll",()=>{
-//   if(window.scrollY >= 154){
-//     navbar.classList.add('fixed')
-//   }else{
-//     navbar.classList.remove('fixed')
-//   }
-// })
+let navbar = document.querySelector('.navbar');
+window.addEventListener("scroll",()=>{
+  if(window.scrollY >= 154){
+    navbar.classList.add('fixed')
+  }else{
+    navbar.classList.remove('fixed')
+  }
+})
 
 
